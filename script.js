b6158239e0cdeab8158316698335d3acdc5cb687
Christofer Carlsson <christofer.carlsson@medieinstitutet.se>
2020-10-13 14:56:24 +0200

    Added the program

---
+++ script.js
@@ -0,0 +1,241 @@
+let loadPicture = function(event) {
+	let image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+// class orderInfo {
+// 	constructor(Size, Amount, basePrice, addonPrice, shippingPrice, totalPrice, firstName, lastName, Adress, Ort, Postcode, Email, Mobile) {
+// 		Size;
+// 		Amount;
+// 		basePrice;
+// 		addonPrice;
+// 		shippingPrice;
+// 		totalPrice;
+
+// 		firstName;
+// 		lastName;
+// 		Adress;
+// 		Ort;
+// 		Postcode;
+// 		Email;
+// 		Mobile;
+// 	}
+// 	subject() {
+// 		"En beställning har blivit gjord!"
+// 	}
+// 	html() { 
+// 		`<h1>En order har gjorts!</h1>` +
+// 		`<h3>en order har blivit gjord och uppgifterna hittar du nedanför!</h3>` +
+// 		`<br />` +
+// 		`<p>Storlek: ${Size}</p>` +
+// 		`<p>Antal: ${Amount}</p>` +
+// 		`<p>Baspris: ${basePrice}</p>` +
+// 		`<p>Tillägg pris: ${addonPrice}</p>` +
+// 		`<p>Frakt pris: ${shippingPrice}</p>` +
+// 		`<h3>Total Summa: ${totalPrice}</h3>` +
+// 		`<br />` +
+// 		`<p>Förnamn: ${firstName}</p>` +
+// 		`<p>Efternamn: ${lastName}</p>` +
+// 		`<p>Adress: ${Adress}</p>` +
+// 		`<p>Ort: ${Ort}</p>` +
+// 		`<p>Postnummer: ${Postcode}</p>` +
+// 		`<p>E-post: ${Email}</p>` +
+// 		`<p>Mobilnummer: ${Mobile}</p>`
+// 	}
+
+// }
+
+
+function sendInfo() {
+	let theSize = document.getElementById("addSizeInput").value;
+	let theAmount = document.getElementById("addAmountInput").value;
+
+	let shippingOption = document.getElementById("shippingOption").value;
+
+	// let userFirstName = document.getElementById("userFirstName").value;
+	// let userLastName = document.getElementById("userLastName").value;
+	// let userAdress = document.getElementById("userAdress").value;
+	// let userOrt = document.getElementById("userOrt").value;
+	// let userPostcode = document.getElementById("userPostcode").value;
+	// let userEmail = document.getElementById("userEmail").value;
+	// let userMobile = document.getElementById("userMobile").value;
+
+	theSize = convertToMM(theSize);
+
+	let priceOfSize = getPriceFromArray(theSize, theAmount);
+	let addonPrice = calculateAddons(priceOfSize);
+	let shippingPrice = checkShippingPrice(shippingOption);
+
+	console.log(priceOfSize)
+	console.log(addonPrice)
+	console.log(shippingPrice)
+
+	let thePrice = priceOfSize + addonPrice;
+	thePrice = thePrice * theAmount;
+	thePrice = thePrice + shippingPrice;
+
+	// alert(`the price is ${thePrice}:-`);
+
+	displayTheResultInPopup(theSize, theAmount, priceOfSize, shippingPrice, addonPrice, thePrice)
+}
+
+function convertToMM(size) {
+	// let sizeInMM = size.substring(0, size.length - 1);
+	let sizeInMM = size * 10;
+	sizeInMM = (Math.floor(sizeInMM / 10) * 10);
+	return sizeInMM;
+}
+
+function getPriceFromArray(size, amount) {
+	let priceOfSize;
+
+	if (size < 40) {
+		alert("Numret måste vara högre än 4")
+		priceOfSize = 0;
+	}
+	if (size == 40) {
+		priceOfSize = brodyrPrice[0];
+	}
+	if (size == 50) {
+		priceOfSize = brodyrPrice[1];
+	}
+	if (size == 60) {
+		priceOfSize = brodyrPrice[2];
+	}
+	if (size == 70) {
+		priceOfSize = brodyrPrice[3];
+	}
+	if (size == 80) {
+		priceOfSize = brodyrPrice[4];
+	}
+	if (size == 90) {
+		priceOfSize = brodyrPrice[5];
+	}
+	if (size == 100) {
+		priceOfSize = brodyrPrice[6];
+	}
+	if (size == 110) {
+		priceOfSize = brodyrPrice[7];
+	}
+	if (size >= 120) {
+		priceOfSize = brodyrPrice[8];
+	}
+
+
+	if (amount < 50) {
+		alert("please add 50 or more")
+		priceOfSize = 0;
+	}
+
+	if (amount >= 50 && amount < 100) {
+		priceOfSize = priceOfSize[0]
+	}
+	if (amount >= 100 && amount < 200) {
+		priceOfSize = priceOfSize[1]
+	}
+	if (amount >= 200 && amount < 300) {
+		priceOfSize = priceOfSize[2]
+	}
+	if (amount >= 300 && amount < 500) {
+		priceOfSize = priceOfSize[3]
+	}
+	if (amount >= 500 && amount < 1000) {
+		priceOfSize = priceOfSize[4]
+	}
+	if (amount >= 1000) {
+		priceOfSize = priceOfSize[5]
+	}
+
+	return priceOfSize;
+}
+
+function calculateAddons(price) {
+
+	let price1 = 0;
+	let price2 = 0;
+	let price3 = 0;
+	let price4 = 0;
+	let price5 = 0;
+
+	let heatBack = document.getElementById("heatBackBox").checked;
+	let kardborre = document.getElementById("kardborreBox").checked;
+	let selfAttach = document.getElementById("selfAttachBox").checked;
+	let specialThread = document.getElementById("specialThreadBox").checked;
+	let wholeBrodyr = document.getElementById("wholeBrodyrBox").checked;
+
+	if (heatBack) {
+		price1 = heatBackPrice;
+	}
+	if (kardborre) {
+		price2 = kardborrePrice;
+	}
+	if (selfAttach) {
+		price3 = selfAttachPrice;
+	}
+	if (specialThread) {
+		price4 = price * specialThreadPrice;
+		price4 = price4 - price;
+	}
+	if (wholeBrodyr) {
+		price5 = price * wholeBrodyrPrice;
+		price5 = price5 - price;
+	}
+	// console.log(price + " before addons")
+
+	let addonPrice = price1 + price2 + price3 + price4 + price5;
+	price = price + addonPrice;
+
+	// console.log(addonPrice)
+	// console.log(price + " after addons")
+
+	return price;
+}
+
+function checkShippingPrice(shipping) {
+
+	if (shipping == "standard") {
+		shipping = standardPrice;
+	}
+	if (shipping == "express") {
+		shipping = expressPrice
+	}
+	if (shipping == "superExpress") {
+		shipping = superExpressPrice
+	}
+
+	return shipping;
+}
+
+function removePopup() {
+	document.getElementById("theOrderPopup").style.display = "none"
+}
+
+function displayTheResultInPopup(theSize, theAmount, priceOfSize, shippingPrice, addonPrice, thePrice) {
+
+	document.getElementById("theOrderPopup").style.display = "flex"
+
+	let userFirstName = document.getElementById("userFirstName").value;
+	let userLastName = document.getElementById("userLastName").value;
+	let userAdress = document.getElementById("userAdress").value;
+	let userOrt = document.getElementById("userOrt").value;
+	let userPostcode = document.getElementById("userPostcode").value;
+	let userEmail = document.getElementById("userEmail").value;
+	let userMobile = document.getElementById("userMobile").value;
+
+
+	document.getElementById("sizeOutput").innerHTML = theSize + ":-";
+	document.getElementById("amountOutput").innerHTML = theAmount + ":-";
+	document.getElementById("basepriceOutput").innerHTML = priceOfSize + ":-";
+	document.getElementById("delieveryPriceOutput").innerHTML = shippingPrice + ":-";
+	document.getElementById("addonPriceOutput").innerHTML = addonPrice + ":-";
+	document.getElementById("totalPriceOutput").innerHTML = thePrice + ":-";
+	document.getElementById("firstNameOutput").innerHTML = userFirstName;
+	document.getElementById("lastNameOutput").innerHTML = userLastName;
+	document.getElementById("adressOutput").innerHTML = userAdress;
+	document.getElementById("ortOutput").innerHTML = userOrt;
+	document.getElementById("postnumberOutput").innerHTML = userPostcode;
+	document.getElementById("emailOutput").innerHTML = userEmail;
+	document.getElementById("mobileOutput").innerHTML = userMobile;
+
+
+}
---
+++ thePrice.js
@@ -0,0 +1,99 @@
+//Price for addons
+let heatBackPrice = 0.15;
+let kardborrePrice = 0.30;
+let selfAttachPrice = 0.15;
+
+//Percentage addons
+let specialThreadPrice = 1.15;
+let wholeBrodyrPrice = 1.15;
+
+//Delievery Price
+let standardPrice = 0;
+let expressPrice = 500;
+let superExpressPrice = 1000; 
+
+let brodyrPrice = [
+
+    mm40 = [
+        13.00,
+        7.20,
+        5.20,
+        4.40,
+        4.00,
+        3.50
+    ],
+    
+    mm50 = [
+        13.80,
+        8.10,
+        5.80,
+        4.70,
+        4.20,
+        3.85
+    ],
+    
+    mm60 = [
+        14.30,
+        9.00,
+        6.70,
+        5.40,
+        4.70,
+        4.50
+    ],
+    
+    mm70 = [
+        15.40,
+        9.50,
+        7.60,
+        5.90,
+        5.20,
+        4.90
+    ],
+    
+    mm80 = [
+        16.90,
+        9.95,
+        8.35,
+        6.60,
+        5.90,
+        5.30
+    ],
+    
+    mm90 = [
+        22.00,
+        12.85,
+        10.00,
+        7.95,
+        7.20,
+        6.80
+    ],
+    
+    mm100 = [
+        23.50,
+        16.66,
+        11.50,
+        9.50,
+        8.60,
+        8.20
+    ],
+    
+    mm110 = [
+        25.00,
+        18.65,
+        13.30,
+        11.50,
+        10.00,
+        9.60
+    ],  
+        
+    mm120 = [
+        27.00,
+        20.00,
+        15.50,
+        12.90,
+        12.00,
+        10.50
+    ],    
+
+
+];
